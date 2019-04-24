79eb5b25c8a2f191c5bb6288ce627d0e7b4476fe
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2019-04-25 02:29:46 +0530

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { DownloadpageComponent } from './downloadpage/downloadpage.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomepageComponent },
+  { path:'downloads/pie', component: DownloadpageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
