c374411ee972ebb97b00da7d0d1cbf0aad68b18b
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2019-04-25 02:28:57 +0530

    Create header with prebuilts

---
+++ src/app/header-main/header-main.component.spec.ts
@@ -0,0 +1,42 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+} from '@angular/material';
+
+import { HeaderMainComponent } from './header-main.component';
+
+describe('HeaderMainComponent', () => {
+  let component: HeaderMainComponent;
+  let fixture: ComponentFixture<HeaderMainComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeaderMainComponent],
+      imports: [
+        NoopAnimationsModule,
+        LayoutModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderMainComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/header-main/header-main.component.ts
@@ -0,0 +1,43 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { MatSidenav, MAT_DRAWER_DEFAULT_AUTOSIZE } from '@angular/material';
+
+@Component({
+  selector: 'app-header-main',
+  templateUrl: './header-main.component.html',
+  styleUrls: ['./header-main.component.css']
+})
+export class HeaderMainComponent implements OnInit {
+
+  @ViewChild('sidenav') sidenav: MatSidenav;
+  isExpanded = true;
+  showSubmenu: boolean = false;
+  isShowing = false;
+  showSubSubMenu: boolean = false;
+  breakpoint: number;
+
+  toggal() {
+    if(window.innerWidth <= 769){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 780) ? 1 : 4; 
+  }
+
+  onResize(event) {
+    this.breakpoint = (event.target.window.innerWidth <= 400) ? 1 : 4;
+  }
+}
